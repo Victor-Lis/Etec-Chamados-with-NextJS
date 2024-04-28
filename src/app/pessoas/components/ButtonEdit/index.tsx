02226a8bdbe26cf0db6bb1703d0cde76df92f286
Victor-Lis <victorlisbronzo1@gmail.com>
2024-04-28 07:46:09 -0300

    v1.04
    - Informando "use client" nos botões ButtonEdit e ButtonExclude;
    - Adicionando cursor pointer nos botões ButtonEdit e ButtonExclude.

--- src/app/pessoas/components/ButtonEdit/index.tsx
+++ src/app/pessoas/components/ButtonEdit/index.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { FiEdit } from "react-icons/fi";
 
 export default function ButtonEdit() {
  return (
     <button className="mr-3" onClick={() => {}}>
-        <FiEdit size={24} color="#006aff" className="hover:scale-110 duration-300"/>
+        <FiEdit size={24} color="#006aff" className="hover:scale-110 duration-300 cursor-pointer"/>
     </button>
  );
 }
--- src/app/pessoas/components/ButtonExclude/index.tsx
+++ src/app/pessoas/components/ButtonExclude/index.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { FiTrash2 } from "react-icons/fi";
 
 export default function ButtonDelete() {
  return (
     <button className="mr-3" onClick={() => {}}>
-        <FiTrash2 size={24} color="#ff000f" className="hover:scale-110 duration-300"/>
+        <FiTrash2 size={24} color="#ff000f" className="hover:scale-110 duration-300 cursor-pointer"/>
     </button>
  );
 }
