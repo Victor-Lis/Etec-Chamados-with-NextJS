e1e7f263d87564b4362661409ca3fc6c9faf0321
Victor-Lis <victorlisbronzo1@gmail.com>
2024-04-30 23:29:32 -0300

    v1.14
    - Ajustando componente para navegar para tela construída para editar cadastro das pessoas

--- src/app/pessoas/components/ButtonEdit/index.tsx
+++ src/app/pessoas/components/ButtonEdit/index.tsx
@@ -2,11 +2,12 @@
 import { FiEdit } from "react-icons/fi";
 
 import { PersonType } from "@/app/@types/person";
+import { useRouter } from "next/navigation";
 
 export default function ButtonEdit({person}:{person: PersonType}) {
-
+ const router = useRouter()
  return (
-    <button className="mr-3" onClick={() => {}}>
+    <button className="mr-3" onClick={() => router.push(`/pessoas/atualizar/${person.key}`)}>
         <FiEdit size={24} color="#006aff" className="hover:scale-110 duration-300 cursor-pointer"/>
     </button>
  );
