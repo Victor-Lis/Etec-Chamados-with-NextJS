b3487c4af9c5a10cb9b5858523bd4ada6fbac945
Victor-Lis <victorlisbronzo1@gmail.com>
2024-05-05 17:18:33 -0300

    v1.20
    - Ajustando funções handleSetDesk e handleSetPeople

--- src/app/mesas/utils/handleSetDesk.ts
+++ src/app/mesas/utils/handleSetDesk.ts
@@ -2,12 +2,20 @@ import { DeskType } from "@/app/@types/desk";
 import { DataSnapshot } from "firebase/database";
 import { Dispatch, SetStateAction } from "react";
 
-export async function handleSetDesk({snapshot, setValue}:{snapshot: DataSnapshot, setValue: Dispatch<SetStateAction<DeskType[]>>}){
-    let snapshotVal = await snapshot.val();
+export async function handleSetDesk({
+  snapshot,
+  setValue,
+}: {
+  snapshot: DataSnapshot;
+  setValue: Dispatch<SetStateAction<DeskType[]>>;
+}) {
+  let snapshotVal = await snapshot.val();
+  let response: DeskType[] = [];
+
+  if (snapshotVal) {
     let keys: string[] = Object.keys(snapshotVal);
     let data: Omit<DeskType[], "key"> = Object.values(snapshotVal);
 
-    let response: DeskType[] = [];
     keys.map((key, index) => {
       response.push({
         key,
@@ -15,6 +23,7 @@ export async function handleSetDesk({snapshot, setValue}:{snapshot: DataSnapshot
         atendente: data[index].atendente as unknown as string,
       });
     });
-    
-    setValue(response)
   }
+
+  setValue(response);
+}
--- src/app/pessoas/utils/handleSetPeople.ts
+++ src/app/pessoas/utils/handleSetPeople.ts
@@ -2,12 +2,20 @@ import { PersonType } from "@/app/@types/person";
 import { DataSnapshot } from "firebase/database";
 import { Dispatch, SetStateAction } from "react";
 
-export async function handleSetPeople({snapshot, setValue}:{snapshot: DataSnapshot, setValue: Dispatch<SetStateAction<PersonType[]>>}){
-    let snapshotVal = await snapshot.val();
+export async function handleSetPeople({
+  snapshot,
+  setValue,
+}: {
+  snapshot: DataSnapshot;
+  setValue: Dispatch<SetStateAction<PersonType[]>>;
+}) {
+  let snapshotVal = await snapshot.val();
+  let response: PersonType[] = [];
+
+  if (snapshotVal) {
     let keys: string[] = Object.keys(snapshotVal);
     let data: Omit<PersonType[], "key"> = Object.values(snapshotVal);
 
-    let response: PersonType[] = [];
     keys.map((key, index) => {
       response.push({
         key,
@@ -15,6 +23,7 @@ export async function handleSetPeople({snapshot, setValue}:{snapshot: DataSnapsh
         email: data[index].email as unknown as string,
       });
     });
-    
-    setValue(response)
   }
+
+  setValue(response);
+}
