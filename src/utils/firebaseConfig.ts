f562be25e76dd3f4688ae80fcf291c688444bdf3
Victor-Lis <109773129+Victor-Lis@users.noreply.github.com>
2024-05-23 23:14:53 -0300

    v1.35
    - Ajustando firebaseConfig

---
+++ src/utils/firebaseConfig.ts
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID
+};
+
+const app = initializeApp(firebaseConfig);
+  
+const database = getDatabase(app);
+  
+const peopleRef = ref(database, "pessoas/")
+
+const tableRef = ref(database, "mesas/")
+
+const ticketsRef = ref(database, "chamados/")
+
+export { app, database, peopleRef, tableRef, ticketsRef }
